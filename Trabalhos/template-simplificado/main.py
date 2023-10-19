fedbe5a137635785b978fe00fb9264aaa83d7a61
Cleber Salustiano <kleberdejesus45@hotmail.com>
2023-10-19 20:07:22 -0300

    remove: "bfs"

--- Trabalhos/template-simplificado/main.py
+++ Trabalhos/template-simplificado/main.py
@@ -102,54 +102,6 @@ def celulas_vizinhas_livres(celula_atual, labirinto):
     return vizinhos_livres
 
 
-def breadth_first_search(labirinto, inicio, goal, viewer):
-    # nos gerados e que podem ser expandidos (vermelhos)
-    fronteira = deque()
-    # nos ja expandidos (amarelos)
-    expandidos = set()
-
-    # adiciona o no inicial na fronteira
-    fronteira.append(inicio)
-
-    # variavel para armazenar o goal quando ele for encontrado.
-    goal_encontrado = None
-
-    # Repete enquanto nos nao encontramos o goal e ainda
-    # existem para serem expandidos na fronteira. Se
-    # acabarem os nos da fronteira antes do goal ser encontrado,
-    # entao ele nao eh alcancavel.
-    while (len(fronteira) > 0) and (goal_encontrado is None):
-
-        # seleciona o no mais antigo para ser expandido
-        no_atual = fronteira.pop()
-
-        # busca os vizinhos do no
-        vizinhos = celulas_vizinhas_livres(no_atual, labirinto)
-
-        # para cada vizinho verifica se eh o goal e adiciona na
-        # fronteira se ainda nao foi expandido e nao esta na fronteira
-        for v in vizinhos:
-            if v.y == goal.y and v.x == goal.x:
-                goal_encontrado = v
-                # encerra o loop interno
-                break
-            else:
-                if (not esta_contido(expandidos, v)) and (not esta_contido(fronteira, v)):
-                    fronteira.append(v)
-
-        expandidos.add(no_atual)
-
-        # viewer.update(generated=fronteira,
-        #               expanded=expandidos)
-        # viewer.pause()
-
-
-    caminho = obtem_caminho(goal_encontrado)
-    custo   = custo_caminho(caminho)
-
-    return caminho, custo, expandidos
-
-
 def depth_first_search(labirinto, inicio, goal, viewer):
     # nos gerados e que podem ser expandidos (vermelhos)
     fronteira = deque()
@@ -332,7 +284,7 @@ def uniform_cost_search(labirinto, inicio, goal, viewer):
 
 
 def main():
-    for _ in range(2):
+    for _ in range(20):
         SEED = 0  # coloque None no lugar do 42 para deixar aleatorio
         random.seed(SEED)
         N_LINHAS  = 100
@@ -371,8 +323,8 @@ def main():
 
         )
 
-        viewer.update(path=caminho)
-        viewer.pause()
+        # viewer.update(path=caminho)
+        # viewer.pause()
         #----------------------------------------
         # A-Star Search
         #----------------------------------------
@@ -395,8 +347,8 @@ def main():
 
         )
 
-        viewer.update(path=caminho)
-        viewer.pause()
+        # viewer.update(path=caminho)
+        # viewer.pause()
 
         #----------------------------------------
         # Uniform Cost Search (Obs: opcional)
@@ -420,8 +372,8 @@ def main():
 
         )
 
-        viewer.update(path=caminho)
-        viewer.pause()
+        # viewer.update(path=caminho)
+        # viewer.pause()
 
 
 
